1651a91fd126e2dde09ff02feb341eadf75456d2
fazioandrea.f <fazioandrea.f@gmail.com>
2021-04-28 12:40:21 +0200

    listato il footer con vue

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,8 @@ function init() {
   new Vue({
     el: '#app',
     data: {
-      headerItem: ['home', 'services', 'about', 'video', 'blog', 'store']
+      headerItem: ['home', 'services', 'about', 'video', 'blog', 'store'],
+      footerlist: ['The best protein shake', 'Ultimate cardio workout', 'New juices available', 'New juices available', 'Tips to find training partners']
     },
     'methods': {
       log: function log() {
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ function init(){
         el:'#app',
         data:{
             headerItem:['home','services','about','video','blog','store'],
+            footerlist:['The best protein shake','Ultimate cardio workout','New juices available','New juices available','Tips to find training partners']
         },
         'methods':{
              log:function(){
