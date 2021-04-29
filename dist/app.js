6f875eaf0280ac38941a26e2d68fdb99aa20889d
fazioandrea.f <fazioandrea.f@gmail.com>
2021-04-29 12:58:00 +0200

    sincornizzato animazioni e con lo slide

--- dist/app.js
+++ dist/app.js
@@ -12,14 +12,31 @@ function init() {
     el: '#app',
     data: {
       headerItem: ['home', 'services', 'about', 'video', 'blog', 'store'],
-      footerlist: ['The best protein shake', 'Ultimate cardio workout', 'New juices available', 'New juices available', 'Tips to find training partners']
+      footerlist: ['The best protein shake', 'Ultimate cardio workout', 'New juices available', 'New juices available', 'Tips to find training partners'],
+      windowHeight: document.documentElement.scrollHeight,
+      scrollPercent: 0
+    },
+    created: function created() {
+      window.addEventListener('scroll', this.log);
     },
     'methods': {
-      log: function log() {
-        console.log('ciao');
+      log: function log(event) {
+        scrollTopp = document.documentElement.scrollTop;
+        documentHeight = document.body.scrollHeight; // windowHeight= document.documentElement.scrollHeight;
+
+        this.scrollPercent = scrollTopp / documentHeight * 100;
+        console.log(this.scrollTopp, this.documentHeight, this.windowHeight, this.scrollPercent);
+        return this.scrollPercent;
       }
     }
-  });
+  }); // function getScroollPercent(){
+  //    var h = document.documentElement, 
+  //     b = document.body,
+  //     st = 'scrollTop',
+  //     sh = 'scrollHeight';
+  //     console.log(h,h[s,b,b[sh],) 
+  // };
+  // getScroollPercent()
 }
 
 document.addEventListener('DOMContentLoaded', init);
